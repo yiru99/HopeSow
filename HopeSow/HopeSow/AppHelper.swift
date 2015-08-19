16cdcf2a5389b9254ad03f7744dd7bfd19099bba
AdamXiaotCao <xiaotcao@gmail.com>
2015-08-19 16:23:52 -0400

    started login view

---
+++ HopeSow/HopeSow/AppHelper.swift
@@ -0,0 +1,14 @@
+//
+//  AppHelper.swift
+//  HopeSow
+//
+//  Created by XiaotianCao on 8/19/15.
+//  Copyright (c) 2015 Adam Cao. All rights reserved.
+//
+
+import Foundation
+class AppHelper{
+    class func getCenterPoints(view: UIView) -> (CGFloat, CGFloat){
+        return (view.center.x, view.center.y)
+    }
+}
--- HopeSow/HopeSow/LoginViewController.swift
+++ HopeSow/HopeSow/LoginViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    var phoneNumberText:UITextField!
+    var passwordText:UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var (centerX, centerY) = AppHelper.getCenterPoints(self.view)
+        let loginButton = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+        loginButton.frame = CGRectMake(centerX - 50, centerY + 100, 200, 50)
+        loginButton.setTitle("Login", forState: UIControlState.Normal)
+        phoneNumberText = UITextField(frame: CGRectMake(centerX - 50, centerY-200, 200, 50))
+        passwordText = UITextField(frame: CGRectMake(centerX-50, centerY - 100, 200, 50))
+        phoneNumberText.placeholder = "enter your phone number"
+        passwordText.placeholder = "enter your password"
+        self.view.addSubview(phoneNumberText)
+        self.view.addSubview(passwordText)
+        self.view.addSubview(loginButton)
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loginUser(sender:UIButton){
+        
     }
     
 
